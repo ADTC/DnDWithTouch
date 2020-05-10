ef776c32c7ec19235bc9a08491b6874fca6b6a3c
Lennie Araki <laraki@thermal.com>
2020-05-10 13:54:14 -0700

    Try #3

--- main.js
+++ main.js
@@ -87,9 +87,9 @@ window.onload = function () {
 
     function handleTouchMove(e) {
         var touchLocation = e.targetTouches[0];
-        var pageX = Math.round(touchLocation.pageX - 50);
-        var pageY = Math.round(touchLocation.pageY - 50);
-        _('app_status').innerHTML = "Touch " + pageX + "px, " + pageY + "px";
+        var pageX = Math.round(touchLocation.pageX - 50) + "px";
+        var pageY = Math.round(touchLocation.pageY - 50) + "px";
+        _('app_status').innerHTML = "Touch " + pageX + ", " + pageY;
         e.target.style.position = "absolute";
         e.target.style.left = pageX;
         e.target.style.top = pageY;
